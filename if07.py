30a5179e056e7842990c3c4f731255722933da71
fulfil_python <f.turdiyevbek94@gmail.com>
2022-12-29 18:01:00 +0500

    if07

--- if07.py
+++ if07.py
@@ -12,4 +12,15 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a>0 and a%2==1:
+        print('musbat toq raqam')
+    elif a>0 and a%2==0:
+        print('musbat juft raqan')
+    if a%2==1:
+        print('toq raqam')
+    elif a!=0 and a%2==0:
+        print('juft raqan')
+    elif a==0:
+        print('0 ga teng')
+    return 0
+y = main(0)
