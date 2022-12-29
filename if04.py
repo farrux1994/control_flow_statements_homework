5c99dbca624a6ed9ced0c0d6d2c0007fbedd00ab
fulfil_python <f.turdiyevbek94@gmail.com>
2022-12-29 13:24:29 +0500

    if04

--- if04.py
+++ if04.py
@@ -7,5 +7,20 @@ def main(a,b,c):
         c: integer
     returns:
         integer: the number of positive numbers in the given numbers
-    """
-    return
+    """ 
+    if a>0 and b>0 and c>0:
+        print('3 ta')
+    elif a>0 and b<0 and c>0:
+        print('2 ta')
+    elif a>0 and b<0 and c<0:
+        print('1 ta')
+    elif a>0 and b>0 and c<0:
+        print('2 ta')
+    elif a<0 and b>0 and c>0:
+        print('2 ta')
+    elif a<0 and b<0 and c>0:
+        print('1 ta')
+    elif a<0 and b<0 and c<0:
+        print('musbat son yo\'q')
+    return 0
+y = main(-2, 4, 1)
