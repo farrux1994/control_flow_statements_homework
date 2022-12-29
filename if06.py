f87752931931a8fe99a9260e8cb4db9b1efa4d64
fulfil_python <f.turdiyevbek94@gmail.com>
2022-12-29 18:00:35 +0500

    if06

--- if06.py
+++ if06.py
@@ -13,4 +13,20 @@ def main(a,b,c):
     Returns:
         string: string with the result
     """
-    return
+    if a>0 and b>0 and c>0:
+            print('3 ta musbat son bor')
+    elif a>0 and b<0 and c>0:
+        print('2 ta musbat 1 ta manfiy son bor')
+    elif a>0 and b<0 and c<0:
+        print('1 ta musbat 2 ta manfiy son bor')
+    elif a>0 and b>0 and c<0:
+        print('2 ta musbat 1 ta manfiy son bor')
+    elif a<0 and b>0 and c>0:
+        print('2 ta ta musbat 1 ta manfiy son bor')
+    elif a<0 and b<0 and c>0:
+        print('1 ta musbat 2 ta manfiy son bor')
+    elif a<0 and b<0 and c<0:
+        print('musbat va manfiy son yo\'q')
+    return 0
+
+y = main(-2, 4, 1)
