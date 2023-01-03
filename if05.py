d3b02582c918ea751b73b6634233657aae1d393f
farrux1994 <f.turdiyevbek94@gmail.com>
2023-01-03 16:37:22 +0500

    if05

--- if05.py
+++ if05.py
@@ -8,20 +8,20 @@ def main(a,b,c):
     returns:
         integer: the number of negative numbers in the given numbers
     """
-    if a>0 and b>0 and c>0:
-            print('manfiy son yo\'q')
-    elif a>0 and b<0 and c>0:
-        print('1 ta')
-    elif a>0 and b<0 and c<0:
-        print('2 ta')
-    elif a>0 and b>0 and c<0:
-        print('1 ta')
-    elif a<0 and b>0 and c>0:
-        print('1 ta')
-    elif a<0 and b<0 and c>0:
-        print('2 ta')
-    elif a<0 and b<0 and c<0:
-        print('3 ta')
-    return 0
+    ans = ''
+    if a>0 and b<0 and c>0:
+        ans = 'one negative numbers'
+    if a>0 and b<0 and c<0:
+        ans = 'two negative numbers'
+    if a>0 and b>0 and c<0:
+        ans = 'one negative numbers'
+    if a<0 and b>0 and c>0:
+        ans = 'one negative numbers'
+    if a<0 and b<0 and c>0:
+        ans = 'two negative numbers'
+    if a<0 and b<0 and c<0:
+        ans = 'there negative numbers'
+    return ans
 
 y = main(-2, 4, 1)
+print(y)
